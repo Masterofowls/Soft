7b1ebe9dbf0322464c964ad423361393a2c2e3ba
Shtarkman Daniel <danvlad2015@gmail.com>
2024-06-08 22:56:02 +0300

    31

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
 
+// Настройка для обслуживания статических файлов
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'game.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/game', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'Hangman', 'game.html'));
+});
+
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
