3a21c30913232d6f56a70d5e9db98a8633fc2af7
Mikhail Makharia <makharia.study@gmail.com>
2024-06-18 11:06:00 +0300

    Delete old js

--- src/main_game/utils/read.js
+++
@@ -1,9 +0,0 @@
-import readJSON from "./readJSON";
-
-export function readQuestions () {
-    return readJSON("src/main_game/fixtures/questions.json");
-};
-
-export function readUsers () {
-    return readJSON("src/main_game/fixtures/users.json");
-}
